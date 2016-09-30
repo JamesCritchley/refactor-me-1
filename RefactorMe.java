22561ecb7cd149622ab615a47e65f06276d081e9
Callum Davies <callum.davies@utcmediacityuk.org.uk>
2016-09-30 14:38:00 +0100

    Update RefactorMe.java

--- RefactorMe.java
+++ RefactorMe.java
@@ -6,18 +6,16 @@ public class RefactorMe {
     public static void main(String[] args) {
         getName();
         calculateSalary(2500, 400);
-        calculateSides("square");
+        calculateSides("circle");
         countLetters();
     }
 
     public static void getName(){
-        String name = "Jay";
-        System.out.println(name);
+        System.out.println("Your age is: ");
     }
 
     public static void calculateSalary(int income, int tax) {
         // Salary = income - tax
-        int salary = income - tax;
     }
 
     public static void calculateSides(String shape) {
@@ -30,27 +28,15 @@ public class RefactorMe {
         if (shape.contains("square")) {
             System.out.println("Your shape has 4 sides");
         }
-        else if (shape.contains("circle")) {
-            System.out.println("Your shape has 1 side");
-        }
-        else if (shape.contains("hexagon")) {
-            System.out.println("Your shape has 6 sides");
-        }
-        else if (shape.contains("dodecagon")) {
-            System.out.println("Your shape has 12 sides");
-        }
-        else {
-            System.out.println("Please enter a valid shape");
-        }
     }
 
     public static void countLetters() {
-        Scanner input = new Scanner(System.in);
-        String word = input.next();
+
+
         //String word = 
         System.out.println("You entered: " + word );
 
-        System.out.println("You word is: " + word.length() + " letters long.");
+        System.out.println("You word is: " + word.length() + "letters long.");
     }
 
 }
